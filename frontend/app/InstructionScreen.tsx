e5622435c1b001c8dc66787cc733ea78f5155871
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2024-11-03 02:41:37 -0600

    instructions styled

--- frontend/app/InstructionScreen.tsx
+++ frontend/app/InstructionScreen.tsx
@@ -1,106 +1,134 @@
-import { useLocalSearchParams } from "expo-router";
-import { SafeAreaView, Text, StyleSheet, Image, Button, TouchableOpacity, View, ScrollView } from "react-native";
-import { router } from "expo-router";
-import BackArrow  from '@/components/BackArrow';
+import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView, Text, StyleSheet, Image, Button, TouchableOpacity, View, ScrollView } from 'react-native';
+import { router } from 'expo-router';
+import BackArrow from '@/components/BackArrow';
 import * as SecureStore from 'expo-secure-store';
 import api from '@/services/AxiosConfig';
-import { useEffect, useState } from "react";
-import Loader from "@/components/Loader";
+import { useEffect, useState } from 'react';
+import Loader from '@/components/Loader';
+import Banner from '@/components/bg/Banner';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const InstructionScreen = () => {
-    const [recipeInstructions, setRecipeInstructions] = useState([])
-    const [ingredients, setIngredients] = useState([])
-    const [init, setInit] = useState(true);
+	const [recipeInstructions, setRecipeInstructions] = useState([]);
+	const [ingredients, setIngredients] = useState([]);
+	const [init, setInit] = useState(true);
 
-    const {data} = useLocalSearchParams();
-    let dataString = Array.isArray(data) ? data.join("") : data;
-    let temp = dataString.split(",")
-   
-    useEffect(() => {
-        requestRecipeDetails();
-      }, []);
+	const { data } = useLocalSearchParams();
+	let dataString = Array.isArray(data) ? data.join('') : data;
+	let temp = dataString.split(',');
 
-    const requestRecipeDetails = async () => {
-        try {
-            const token = await SecureStore.getItem('token');
-            const response = await api.get('/recipe-details', {
-                params: {
-                    token: token,
-                    id: temp[0]
-                }
-            });
-            setRecipeInstructions(response.data.analyzedInstructions[0].steps);
-            setIngredients(response.data.extendedIngredients);
-            console.log(recipeInstructions);
-            setInit(false);
-        } catch (error: any) {
-            console.error('Error fetching recipes:', error.response.data)
-        }
-    };
+	useEffect(() => {
+		requestRecipeDetails();
+	}, []);
 
-    if (init) {
-        return <Loader/>
-    }
-    
-    return (
-        <SafeAreaView>
-            <View style={styles.header}>
-        <BackArrow/>
-            </View>
-            <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
-                <Text style={styles.recipeTitle}>{temp[1]}</Text>
-                <Image source={{ uri: temp[2]}} style={styles.recipeImage} />
-                <Text style={styles.recipeHeader}>Ingredients</Text>
-                {/* Ingredients go here */}
-                {ingredients.map((ingredient: any, i) => (
-                    <Text style={styles.recipeText} key={i}>{"\u2022"} {ingredient.original}</Text>
-                ))}
-                
-                <Text style={styles.recipeHeader}>Directions</Text>
-                {recipeInstructions.map((recipeInstruction: any, i) => (
-                    <Text style={styles.recipeText} key={i}>{"\u2022"} {recipeInstruction.step}</Text>
-                ))}
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
- 
+	const requestRecipeDetails = async () => {
+		try {
+			const token = SecureStore.getItem('token');
+			const response = await api.get('/recipe-details', {
+				params: {
+					token: token,
+					id: temp[0],
+				},
+			});
+			setRecipeInstructions(response.data.analyzedInstructions[0].steps);
+			setIngredients(response.data.extendedIngredients);
+			setInit(false);
+		} catch (error: any) {
+			console.error('Error fetching recipes:', error.response.data);
+		}
+	};
+
+	if (init) {
+		return <Loader />;
+	}
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Banner style={{ position: 'absolute', top: -150, zIndex: 5 }}></Banner>
+			<View style={styles.header}>
+				<Ionicons
+					name="chevron-back-outline"
+					size={30}
+					color="#fff"
+					style={{ zIndex: 10 }}
+					onPress={() => {
+                        if (router.canGoBack()) {
+                            router.back();
+						}
+					}}
+				/>
+			</View>
+            <Text style={styles.recipeTitle}>{temp[1]}</Text>
+			<ScrollView>
+				<Image source={{ uri: temp[2] }} style={styles.recipeImage} />
+				<Text style={styles.recipeHeader}>Ingredients</Text>
+				{ingredients.map((ingredient: any, i) => (
+					<Text style={styles.recipeText} key={i}>
+						{'\u2022'} {ingredient.original}
+					</Text>
+				))}
+
+				<Text style={styles.recipeHeader}>Directions</Text>
+				{recipeInstructions.map((recipeInstruction: any, i) => (
+					<Text style={styles.recipeText} key={i}>
+						{i + 1}. {recipeInstruction.step}
+					</Text>
+				))}
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
+
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        paddingLeft: 25,
-        paddingTop: 10
-    },
-    recipeTitle: {
-        fontSize: 36,
-        padding: 30,
-    },
-    recipeHeader: {
-        fontSize: 36,
-        color: "#6DC47E",
-        padding: 30,
-    },
-    recipeText: {
-        fontSize: 14,
-        paddingLeft: 55,
-        paddingRight: 20,
-    },
-    recipeImage: {
-        width: 200,
-        height: 150,
-        alignSelf: 'center'
-    },
-    backBtnText: {
-        color: "#439C54"
-    },
-    backButton: {
-        paddingLeft: 20,
-        paddingTop: 45,
-    },
-    backArrowContainer: {
-        flexDirection: 'row', 
-        alignItems: 'center', 
-    },
-  });
+	container: {
+		flex: 1,
+	},
+	header: {
+		width: '100%',
+		paddingLeft: 25,
+		paddingTop: 10,
+        zIndex: 10
+	},
+	recipeTitle: {
+		fontSize: 30,
+		padding: 5,
+		alignSelf: 'center',
+		fontFamily: 'JostSemiBold',
+		color: '#fff',
+        zIndex: 10,
+        textAlign: 'center'
+	},
+	recipeHeader: {
+		fontSize: 32,
+		color: '#6DC47E',
+		paddingTop: 15,
+        paddingBottom: 10,
+        paddingLeft: 30,
+	},
+	recipeText: {
+		fontSize: 14,
+        fontFamily: 'JostRegular',
+		paddingLeft: 50,
+		paddingRight: 20,
+        color: '5E5E5E'
+	},
+	recipeImage: {
+		width: 500,
+		height: 250,
+		alignSelf: 'center',
+	},
+	backBtnText: {
+		color: '#439C54',
+	},
+	backButton: {
+		paddingLeft: 20,
+		paddingTop: 45,
+	},
+	backArrowContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+});
 
 export default InstructionScreen;
